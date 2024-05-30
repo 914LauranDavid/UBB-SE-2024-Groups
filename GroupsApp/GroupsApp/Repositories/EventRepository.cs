a70d5bd5e651c8fbb8173eed40426ca0eb55eea3
Lorintiu Daniel <lorintiu.daniel@yahoo.com>
2024-05-30 20:50:15 +0300

    events repositories

---
+++ GroupsApp/GroupsApp/Repositories/EventRepository.cs
@@ -0,0 +1,62 @@
+﻿using GroupsApp.Data;
+using GroupsApp.Models;
+
+namespace GroupsApp.Repositories
+{
+    public class EventRepository(GroupsAppContext context)
+    {
+        private readonly GroupsAppContext _context = context;
+
+        public Event AddEvent(Event marketplaceEvent)
+        {
+            _context.Events.Add(marketplaceEvent);
+            _context.SaveChanges();
+            return marketplaceEvent;
+        }
+
+        public Event? GetEventById(Guid id)
+        {
+            return _context.Events.Find(id);
+        }
+
+        public Event UpdateEvent(Event marketplaceEvent)
+        {
+            Event? foundEvent = _context.Events.Find(marketplaceEvent.EventId);
+            if (foundEvent == null)
+            {
+                throw new Exception("Event not found");
+            }
+            foundEvent.OrganizerId = marketplaceEvent.OrganizerId;
+            foundEvent.EventName = marketplaceEvent.EventName;
+            foundEvent.Categories = marketplaceEvent.Categories;
+            foundEvent.Location = marketplaceEvent.Location;
+            foundEvent.MaxParticipants = marketplaceEvent.MaxParticipants;
+            foundEvent.Description = marketplaceEvent.Description;
+            foundEvent.StartDate = marketplaceEvent.StartDate;
+            foundEvent.EndDate = marketplaceEvent.EndDate;
+            foundEvent.BannerURL = marketplaceEvent.BannerURL;
+            foundEvent.LogoURL = marketplaceEvent.LogoURL;
+            foundEvent.AgeLimit = marketplaceEvent.AgeLimit;
+            foundEvent.EntryFee = marketplaceEvent.EntryFee;
+            Event updatedEvent = _context.Events.Update(foundEvent).Entity;
+            _context.SaveChanges();
+            return updatedEvent;
+        }
+
+        public void DeleteEvent(Event marketplaceEvent)
+        {
+            Event? foundEvent = _context.Events.Find(marketplaceEvent.EventId);
+            if (foundEvent == null)
+            {
+                throw new Exception("Event not found");
+            }
+            _context.Events.Remove(foundEvent);
+            _context.SaveChanges();
+        }
+
+        public List<Event> GetAllEvents()
+        {
+            return _context.Events.ToList();
+        }
+    }
+}
---
+++ GroupsApp/GroupsApp/Repositories/UserEventRepository.cs
@@ -0,0 +1,66 @@
+﻿using GroupsApp.Data;
+using GroupsApp.Models;
+
+namespace GroupsApp.Repositories
+{
+    public class UserEventRepository
+    {
+        private readonly GroupsAppContext _context;
+
+        public UserEventRepository(GroupsAppContext context)
+        {
+            _context = context;
+        }
+
+        public UserEvent AddUserEvent(UserEvent userEvent)
+        {
+            _context.UserEvents.Add(userEvent);
+            _context.SaveChanges();
+            return userEvent;
+        }
+
+        public UserEvent? GetUserEventById(Guid userId, Guid eventId)
+        {
+            return _context.UserEvents.SingleOrDefault(ue => ue.UserId == userId && ue.EventId == eventId);
+        }
+
+        public UserEvent UpdateUserEvent(UserEvent userEvent)
+        {
+            UserEvent? foundUserEvent = _context.UserEvents.SingleOrDefault(ue => ue.UserId == userEvent.UserId && ue.EventId == userEvent.EventId);
+            if (foundUserEvent == null)
+            {
+                throw new Exception("UserEvent not found");
+            }
+            foundUserEvent.Status = userEvent.Status;
+            UserEvent updatedUserEvent = _context.UserEvents.Update(foundUserEvent).Entity;
+            _context.SaveChanges();
+            return updatedUserEvent;
+        }
+
+        public void DeleteUserEvent(Guid userId, Guid eventId)
+        {
+            UserEvent? foundUserEvent = _context.UserEvents.SingleOrDefault(ue => ue.UserId == userId && ue.EventId == eventId);
+            if (foundUserEvent == null)
+            {
+                throw new Exception("UserEvent not found");
+            }
+            _context.UserEvents.Remove(foundUserEvent);
+            _context.SaveChanges();
+        }
+
+        public List<UserEvent> GetAllUserEvents()
+        {
+            return _context.UserEvents.ToList();
+        }
+
+        public List<UserEvent> GetUserEventsByUserId(Guid userId)
+        {
+            return _context.UserEvents.Where(ue => ue.UserId == userId).ToList();
+        }
+
+        public List<UserEvent> GetUserEventsByEventId(Guid eventId)
+        {
+            return _context.UserEvents.Where(ue => ue.EventId == eventId).ToList();
+        }
+    }
+}
