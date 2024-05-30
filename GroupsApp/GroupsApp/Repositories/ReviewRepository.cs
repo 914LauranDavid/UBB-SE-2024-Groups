b067cadf5a1c8329c01e46a69c8a3891e9e12d5e
Gulin7 <gulintudor@gmail.com>
2024-05-30 19:30:23 +0300

    Update ReviewRepository.cs

--- GroupsApp/GroupsApp/Repositories/ReviewRepository.cs
+++ GroupsApp/GroupsApp/Repositories/ReviewRepository.cs
@@ -1,6 +1,53 @@
-﻿namespace GroupsApp.Repositories
+﻿using GroupsApp.Data;
+using GroupsApp.Models;
+
+namespace GroupsApp.Repositories
 {
-    public class ReviewRepository
+    public class ReviewRepository(GroupsAppContext context)
     {
+        private readonly GroupsAppContext _context = context;
+
+        /*
+        public Review AddReview(Review review)
+        {
+            Review savedReview = _context.Reviews.Add(review).Entity;
+            _context.SaveChanges();
+            return savedReview;
+        }
+
+        public Review? GetReviewById(Guid reviewId)
+        {
+            return _context.Reviews.Find(reviewId);
+        }
+
+        public Review UpdateReview(Review review)
+        {
+            Review? foundReview = _context.Reviews.Find(review.ReviewId);
+            if (foundReview != null)
+            {
+                throw new Exception("Review not found");
+            }
+            foundReview.Content = review.Content;
+            foundReview.Rating = review.Rating;
+            Review updatedReview = _context.Reviews.Update(review).Entity;
+            _context.SaveChanges();
+            return updatedReview;
+        }
+
+        public void DeleteReview(Review review) {
+            Review? foundReview = _context.Reviews.Find(review.ReviewId);
+            if (foundReview != null)
+            {
+                throw new Exception("Review not found");
+            }
+            _context.Reviews.Remove(review);
+            _context.SaveChanges();
+        }
+
+        public List<Review> GetAllReviews()
+        {
+            return [.. _context.Reviews];
+        }
+        */
     }
 }
