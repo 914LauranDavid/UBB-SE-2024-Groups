03aa6044e17daafb8961e7ea6ada076e11ef02ca
Gulin7 <gulintudor@gmail.com>
2024-05-31 11:07:40 +0300

    Update EventController.cs

--- GroupsApp/GroupsApp/Controllers/EventController.cs
+++ GroupsApp/GroupsApp/Controllers/EventController.cs
@@ -1,12 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GroupsApp.Models;
+using GroupsApp.Models.MarketplacePosts;
+using GroupsApp.Payload.DTO;
+using GroupsApp.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GroupsApp.Controllers
 {
+    [Route("/event")]
     public class EventController : Controller
     {
-        public IActionResult Index()
+        
+        private readonly IEventService eventService;
+
+        public EventController(IEventService eventService)
         {
-            return View();
+            this.eventService = eventService;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Event>>> GetAllEvents()
+        {
+            return eventService.GetAllEvents();
+        }
+
+        [HttpDelete("{eventId}")]
+        public async Task<IActionResult> DeleteEvent(Guid eventId)
+        {
+            try
+            {
+                this.eventService.DeleteEventById(eventId);
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet("{eventId}")]
+        public async Task<ActionResult<Event>> GetEvent(Guid eventId)
+        {
+            try
+            {
+                return Ok(eventService.GetEventById(eventId));
+                
+            }catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
     }
 }
+
+/*
+ *         [HttpGet("{eventId}")]
+        public ActionResult<Event> GetEventById(Guid eventId)
+        {
+            try
+            {
+                var event = eventService.GetEventById(eventId);
+                return Ok(event);
+            }catch (Exception ex)
+            {
+                return NotFound(ex.Message);
+    }
+*/
