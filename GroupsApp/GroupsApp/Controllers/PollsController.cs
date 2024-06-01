4aad1deb113d2d3aa13b3bc9084213805c66593e
RLSMname <115074855+RLSMname@users.noreply.github.com>
2024-06-01 13:45:34 +0300

    emi - get group polls

---
+++ GroupsApp/GroupsApp/Controllers/PollsController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GroupsApp.Data;
+using GroupsApp.Models;
+
+namespace GroupsApp.Controllers
+{
+    public class PollsController : Controller
+    {
+        private readonly GroupsAppContext _context;
+        private readonly IPollService _pollService;
+
+        public PollsController(GroupsAppContext context, IPollService pollService)
+        {
+            _context = context;
+            _pollService = pollService;
+        }
+
+
+        // GET: Polls/GroupPolls/5
+        public async Task<IActionResult> GroupPolls(Guid? groupId)
+        {
+            if (groupId == null)
+            {
+                return NotFound();
+            }
+
+            var pollsFromGroup = _pollService.GetGroupPolls((Guid)groupId);
+            if (pollsFromGroup == null)
+            {
+                return NotFound();
+            }
+
+            return View(pollsFromGroup);
+        }
+
+        // GET: Polls/Create
+        public IActionResult Create()
+        {
+            ViewData["GroupId"] = new SelectList(_context.Groups, "GroupId", "GroupId");
+            return View();
+        }
+
+        // POST: Polls/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PollId,GroupId,Description,EndDate,IsPinned,IsVisible,IsMultipleChoice,IsAnonymous")] Poll poll)
+        {
+            if (ModelState.IsValid)
+            {
+                poll.PollId = Guid.NewGuid();
+                _context.Add(poll);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["GroupId"] = new SelectList(_context.Groups, "GroupId", "GroupId", poll.GroupId);
+            return View(poll);
+        }
+
+}
