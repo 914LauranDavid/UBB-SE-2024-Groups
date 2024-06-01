7c666445f94bdc49a2e8e0035b99e7641398c4de
RLSMname <115074855+RLSMname@users.noreply.github.com>
2024-06-01 11:42:37 +0300

    added generated cartscontroller

---
+++ GroupsApp/GroupsApp/Controllers/CartsController.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using GroupsApp.Data;
+using GroupsApp.Models;
+
+namespace GroupsApp.Controllers
+{
+    public class CartsController : Controller
+    {
+        private readonly GroupsAppContext _context;
+
+        public CartsController(GroupsAppContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Carts
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Cart.ToListAsync());
+        }
+
+        // GET: Carts/Details/5
+        public async Task<IActionResult> Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // GET: Carts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Carts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,MarketplacePostId")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                cart.UserId = Guid.NewGuid();
+                _context.Add(cart);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cart);
+        }
+
+        // GET: Carts/Edit/5
+        public async Task<IActionResult> Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+            return View(cart);
+        }
+
+        // POST: Carts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, [Bind("UserId,MarketplacePostId")] Cart cart)
+        {
+            if (id != cart.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cart);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartExists(cart.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cart);
+        }
+
+        // GET: Carts/Delete/5
+        public async Task<IActionResult> Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // POST: Carts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(Guid id)
+        {
+            var cart = await _context.Cart.FindAsync(id);
+            if (cart != null)
+            {
+                _context.Cart.Remove(cart);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartExists(Guid id)
+        {
+            return _context.Cart.Any(e => e.UserId == id);
+        }
+    }
+}
