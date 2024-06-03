5c184d95ced91112df7b6469c49b6727f6e20762
David Seu 916 <david.seu@stud.ubbcluj.ro>
2024-06-03 11:03:20 +0300

    Update EventMapper.cs

--- GroupsApp/GroupsApp/Mapper/EventMapper.cs
+++ GroupsApp/GroupsApp/Mapper/EventMapper.cs
@@ -3,18 +3,18 @@ using GroupsApp.Payload.DTO;
 
 namespace GroupsApp.Mapper
 {
-    public class EventMapper
+    public static class EventMapper
     {
         public static Event EventDTOToEvent(EventDTO eventDTO)
         {
             return new Event(eventDTO.EventId, eventDTO.OrganizerId, eventDTO.EventName, eventDTO.Categories, eventDTO.Location, eventDTO.MaxParticipants, eventDTO.Description, eventDTO.StartDate, eventDTO.EndDate, eventDTO.BannerURL, eventDTO.LogoURL, eventDTO.AgeLimit, eventDTO.EntryFee);
         }
 
-        /*public static EventDTO EventToEventDTO(Event event)
+        public static EventDTO EventToEventDTO(Event eventX)
         {
             EventDTO eventDTO = new EventDTO();
-            eventDTO.EventId = event.EventId;
+            eventDTO.EventId = eventX.EventId;
             return eventDTO;
-        }*/
+        }
     }
 }
